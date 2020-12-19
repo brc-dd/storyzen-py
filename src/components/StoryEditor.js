6abc79a35dd762aa0fae5e9317cf8b0e3435f830
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2020-12-19 20:24:04 +0530

    feat: add support for read-only mode

--- src/components/StoryEditor.js
+++ src/components/StoryEditor.js
@@ -1,31 +1,63 @@
-// eslint-disable
-
 import 'assets/styles/_editor.scss';
 
 import EditorJS from '@editorjs/editorjs';
 import DragDrop from 'editorjs-drag-drop';
 import Undo from 'editorjs-undo';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Button, Container, Row } from 'react-bootstrap';
 import EDITOR_JS_TOOLS from 'utils/tools';
 
 export default () => {
+  const readOnly = true;
+
   const editor = new EditorJS({
     tools: EDITOR_JS_TOOLS,
     inlineToolbar: true,
     placeholder: "Let's write an awesome story!",
     onReady: () => {
-      new Undo({ editor });
-      new DragDrop(editor);
-      // eslint-disable-next-line no-console
-      console.clear();
+      if (!readOnly) {
+        new Undo({ editor });
+        new DragDrop(editor);
+      }
     },
-    logLevel: 'ERROR'
+    logLevel: 'ERROR',
+    data: {
+      time: 1608370653638,
+      blocks: [
+        {
+          type: 'paragraph',
+          data: {
+            text: 'Hey there! This is an article.'
+          }
+        }
+      ],
+      version: '2.19.1'
+    },
+    readOnly
   });
+
+  const handleClick = () => {
+    editor
+      .save()
+      .then(outputData => console.log('Article data: ', outputData))
+      .catch(error => console.log('Saving failed: ', error));
+  };
 
   return (
     <Container className="pt-5">
       <div id="editorjs" className="mt-4" />
+      {!readOnly ? (
+        <Row>
+          <Button
+            variant="dark"
+            size="lg"
+            className="mx-auto px-5"
+            onClick={handleClick}
+          >
+            Publish!
+          </Button>
+        </Row>
+      ) : null}
     </Container>
   );
 };
