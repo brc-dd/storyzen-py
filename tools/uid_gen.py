44ec82526326bb6d1062d121f1f024260c147a8a
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-02 00:30:59 +0530

    fix: freeze requirements.txt and minor fixes

--- tools/uid_gen.py
+++ tools/uid_gen.py
@@ -6,10 +6,10 @@ from slugify import slugify
 
 def generate_uid(data: dict) -> str:
     try:
-        uid = slugify(data["blocks"][0]["data"]["text"], max_length=54)
+        uid = slugify(data['blocks'][0]['data']['text'], max_length=54)
         if len(uid) < 6:
             raise Exception('very short uid')
-        return uid + ('-' if uid[-1] != '-' else '') + str(data["time"])[4:]
+        return uid + ('-' if uid[-1] != '-' else '') + str(data['time'])[4:]
     except:
         traceback.print_exc()
         return uuid.uuid4().hex
