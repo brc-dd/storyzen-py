59e8cbd832a3d7dd4d971756d1e4b87d1006f2f4
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-03 21:37:06 +0530

    fix: minor changes, start frontend with backend

--- server.ps1
+++ server.ps1
@@ -2,5 +2,6 @@ if (!(Get-Process Insomnia -ErrorAction SilentlyContinue))
 { Start-Process "$ENV:LocalAppData\insomnia\Insomnia" }
 if (!(Get-Process robo3t -ErrorAction SilentlyContinue)) 
 { Start-Process 'C:\Program Files\Robo 3T 1.4.2\robo3t' }
-(Get-Process mongod | Stop-Process) 2>$null; Start-Process mongod
-& .\venv\Scripts\Activate.ps1; python .\app.py
+(Get-Process mongod | Stop-Process) 2>$null
+& .\venv\Scripts\Activate.ps1; Clear-Host
+concurrently -k "mongod" "python ./app.py" "yarn --cwd ../frontend/ start"
