7f35bb04d85e081d7a5ec8cb5f67cdf9a152f514
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-01-03 22:01:10 +0530

    chore: add styling to concurrently

--- server.ps1
+++ server.ps1
@@ -1,7 +1,9 @@
-if (!(Get-Process Insomnia -ErrorAction SilentlyContinue)) 
-{ Start-Process "$ENV:LocalAppData\insomnia\Insomnia" }
-if (!(Get-Process robo3t -ErrorAction SilentlyContinue)) 
-{ Start-Process 'C:\Program Files\Robo 3T 1.4.2\robo3t' }
+if ($Debug -eq $true) {
+    if (!(Get-Process Insomnia -ErrorAction SilentlyContinue)) 
+    { Start-Process "$ENV:LocalAppData\insomnia\Insomnia" }
+    if (!(Get-Process robo3t -ErrorAction SilentlyContinue)) 
+    { Start-Process 'C:\Program Files\Robo 3T 1.4.2\robo3t' }
+}
 (Get-Process mongod | Stop-Process) 2>$null
 & .\venv\Scripts\Activate.ps1; Clear-Host
-concurrently -k "mongod" "python ./app.py" "yarn --cwd ../frontend/ start"
+concurrently -n 'Database,Backend,Frontend' -c 'bgBlue.bold,bgMagenta.bold,bgCyan.bold' -k 'mongod' 'python ./app.py' 'yarn --cwd ../frontend/ start'
